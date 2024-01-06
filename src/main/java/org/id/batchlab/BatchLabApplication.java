21952d3efbf26380079c4b4d54deed2510e21d54
Saif <117396354+Wulfin@users.noreply.github.com>
2024-01-06 23:27:33 +0200

    Initial commit

---
+++ src/main/java/org/id/batchlab/BatchLabApplication.java
@@ -0,0 +1,13 @@
+package org.id.batchlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/id/batchlab/BatchLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.id.batchlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
